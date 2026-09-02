86bc720365e91a508f7b5f9de395d2f243fea91e
mdshiblinoman <mdshiblisharkar@gmail.com>
2026-09-02 16:49:18 +0600

    upload solving problem 3 problem

--- problem_3.c
+++ problem_3.c
@@ -18,174 +18,60 @@ P3 receives 100 from P2
 P3 sends 100 to P0
 P0 receives 100 from P3
 Ring communication completed
-
-run project = /usr/bin/mpicc problem_3.c -o problem_3
-then /usr/bin/mpirun -np 4 ./problem_3
 */
 
 #include <stdio.h>
-#include <stdlib.h>
 #include <mpi.h>
 
 int main(int argc, char *argv[])
 {
     int rank, size;
+    int previous, next;
+    int value = 0;
 
     MPI_Init(&argc, &argv);
-
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    /* Need at least 2 processes */
-    if (size < 2)
+    if (size != 4)
     {
         if (rank == 0)
-        {
-            printf("Please run with at least 2 processes.\n");
-        }
+            printf("Run the program with exactly 4 processes.\n");
 
         MPI_Finalize();
         return 0;
     }
 
-    // Ring topology
-    //  Previous process
-    int previous = (rank - 1 + size) % size;
+    previous = (rank - 1 + size) % size;
+    next = (rank + 1) % size;
 
-    // Next process
-    int next = (rank + 1) % size;
-
-    // Message
-    int value = 0;
-    int received_value = 0;
-    int sent_value = 0;
-
-    // P0 starts with 100
     if (rank == 0)
     {
         value = 100;
-    }
-
-    // Synchronize all processes
-    MPI_Barrier(MPI_COMM_WORLD);
-
-    // Start communication timer
-    double start_time = MPI_Wtime();
-
-    /*
-     * Even processes send first.
-     * Odd processes receive first.
-     *
-     * This prevents deadlock with blocking
-     * MPI_Send and MPI_Recv.
-     */
-
-    if (rank % 2 == 0)
-    {
-        sent_value = value;
-
-        // Send to next process
-        MPI_Send(
-            &sent_value,
-            1,
-            MPI_INT,
-            next,
-            100,
-            MPI_COMM_WORLD);
-
-        // Receive from previous process
-        MPI_Recv(
-            &received_value,
-            1,
-            MPI_INT,
-            previous,
-            100,
-            MPI_COMM_WORLD,
-            MPI_STATUS_IGNORE);
-
-        if (received_value != 0)
-        {
-            value = received_value;
-        }
-    }
-    else
-    {
-        // Receive from previous process
-        MPI_Recv(
-            &received_value,
-            1,
-            MPI_INT,
-            previous,
-            100,
-            MPI_COMM_WORLD,
-            MPI_STATUS_IGNORE);
-
-        value = received_value;
-        sent_value = value;
-
-        // Send to next process
-        MPI_Send(
-            &sent_value,
-            1,
-            MPI_INT,
-            next,
-            100,
-            MPI_COMM_WORLD);
-    }
-
-    // End timer
-    double end_time = MPI_Wtime();
-
-    double communication_time = end_time - start_time;
-
-    // Print communication in the sample order.
-    if (rank == 0)
-    {
         printf("P0 sends %d to P1\n", value);
         fflush(stdout);
-    }
 
-    MPI_Barrier(MPI_COMM_WORLD);
+        MPI_Send(&value, 1, MPI_INT, next, 100, MPI_COMM_WORLD);
 
-    for (int p = 1; p < size; p++)
-    {
-        if (rank == p)
-        {
-            printf("P%d receives %d from P%d\n", rank, received_value, previous);
-            fflush(stdout);
-        }
+        MPI_Recv(&value, 1, MPI_INT, previous, 100, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 
-        MPI_Barrier(MPI_COMM_WORLD);
-
-        if (rank == p)
-        {
-            printf("P%d sends %d to P%d\n", rank, value, next);
-            fflush(stdout);
-        }
-
-        MPI_Barrier(MPI_COMM_WORLD);
-    }
-
-    if (rank == 0)
-    {
-        printf("P0 receives %d from P%d\n", received_value, previous);
+        printf("P0 receives %d from P3\n", value);
         printf("Ring communication completed\n");
         fflush(stdout);
     }
+    else
+    {
+        MPI_Recv(&value, 1, MPI_INT, previous, 100, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 
-    // Find maximum communication time
-    double max_time;
+        printf("P%d receives %d from P%d\n", rank, value, previous);
+        fflush(stdout);
 
-    MPI_Reduce(
-        &communication_time,
-        &max_time,
-        1,
-        MPI_DOUBLE,
-        MPI_MAX,
-        0,
-        MPI_COMM_WORLD);
+        MPI_Send(&value, 1, MPI_INT, next, 100, MPI_COMM_WORLD);
+
+        printf("P%d sends %d to P%d\n", rank, value, next);
+        fflush(stdout);
+    }
 
     MPI_Finalize();
-
     return 0;
 }
